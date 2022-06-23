2733906b650b5b5f46ca8c4958971a4ff2ce7a61
cheezedoodles <hougvapes@gmail.com>
2022-06-23 15:02:35 +0300

    created models for chats and messages

---
+++ chat/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+    sent_from_id = models.ForeignKey(User,
+                                    on_delete=models.CASCADE,
+                                    related_name='sender')
+    sent_to_id = models.ForeignKey(User, 
+                                  on_delete=models.CASCADE,
+                                  related_name='receiver')
+
+class Message(models.Model):
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE,
+                                related_name='chat')
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+    
+    def __str__(self):
+        return f'Message sent at {self.created} from chat id {self.chat_id}'
